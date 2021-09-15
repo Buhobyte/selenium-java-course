e2d289d17efbfe70fdd6387e8499c16a7fc59f23
buhobyte <byron.lopez.begins@gmail.com>
2021-09-15 18:38:32 -0500

    Finding WebElements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,11 @@
 package base;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
@@ -12,10 +15,12 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        // custom windows chrome
-//        driver.manage().window().maximize();
-//        driver.manage().window().fullscreen();
-        driver.manage().window().setSize(new Dimension(375,812));
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Forgot Password"));
+        inputsLink.click();
+
 
         System.out.println(driver.getTitle());
         driver.close();
